013738f00fedc0d30d3fa678310bcb064bb2a30a
Brianna Marks <saphereshade2@gmail.com>
2017-02-15 16:26:09 -0600

    Search results now display in a table

--- search.js
+++ search.js
@@ -1,26 +1,32 @@
 function searchForSong() {
     var songSearch = $("#songInput").val();
     var searchUrl =  "http://itunes.apple.com/search?term=" + songSearch;
+    var table = $("#displayTable");
 
         $.ajax({
-            type: 'GET',
             url: searchUrl,
-            contentType: "jsonp",
+            method: "GET",
             dataType: "jsonp",
-            async: false,
-            complete: function(data) {
-                displaySongInfo(data.results);
+            success: function(data) {
+
+                console.log("Ya did it", data);
+
+                $.each(data.results, function(index) {
+                    var songName = data.results[index].trackName;
+                    var songArtist = data.results[index].artistName;
+                    var songGenre = data.results[index].primaryGenreName;
+                    //var songPreview = "<audio src='" + data.results[index].previewUrl + "' >";
+                    var songAlbumArt = "<img src='"  + data.results[index].artworkUrl100 + "'>";
+
+                    table.append("<tr><td>" + songAlbumArt + "</td><td>" + songName + "</td><td>" + songArtist + "</td><td>" + songGenre + "</td></tr>");
+                });
+
+
+
             }
         });
 }
 function displaySongInfo(data) {
-    var results = data;
-    var resultsDisplay = $("#displayTable");
-
-    $.each(results, function(key, data){
-        var albumArt = $("#albumCover") + results[key];
-        var songName = $("#song") + results[key].trackName;
-        var artistName = $("#artist") + results[key].artistName;
-    });
+    //"</td><td>" + songPreview +
 }
 
